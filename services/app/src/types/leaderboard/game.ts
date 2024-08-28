642d3d8f3010664dddb43932febf11979734393a
fedorerd <fedor.timohov@gmail.com>
2024-08-28 19:52:28 +0300

    leaderboard

--- services/app/src/types/leaderboard/game.ts
+++ services/app/src/types/leaderboard/game.ts
@@ -3,18 +3,24 @@ import { ApiArrayTypeBuilder } from "../query_multiple"
 
 export type GameLeaderboardMemberApi = {
     username: string,
+    first_name: string,
+    last_name: string,
     game_records: number
 }
 
 export type GameLeaderboardMember = {
     username: string,
+    firstName: string,
+    lastName: string,
     record: number
 }
 
 export const GameLeaderboardMemberApiTypeBuilder = new ApiTypeBuilder<GameLeaderboardMemberApi, GameLeaderboardMember>(
     (i) => ({
         username: i.username,
-        record: i.game_records
+        record: i.game_records,
+        firstName: i.first_name,
+        lastName: i.last_name
     })
 )
 
