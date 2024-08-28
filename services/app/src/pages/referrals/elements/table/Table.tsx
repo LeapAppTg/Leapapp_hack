7acd3ef93ecf8d2e98755626cf879994c8346391
fedorerd <fedor.timohov@gmail.com>
2024-08-28 21:23:31 +0300

    referrals: unused imports

--- services/app/src/pages/referrals/elements/table/Table.tsx
+++ services/app/src/pages/referrals/elements/table/Table.tsx
@@ -3,11 +3,10 @@ import { FlexGapColumn16FullWidth } from "@utils";
 import { FC } from "react";
 import { NoReferrals, TableItem, TableTitle } from "../../components";
 import styles from "./styles.module.css";
-import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 export const Table: FC = () => {
 
-    const { data: referralsList, setSize, isValidating, isLoading } = useData(ApiRoutes.GetReferralsList)
+    const { data: referralsList, setSize, isValidating } = useData(ApiRoutes.GetReferralsList)
     const tableRef = usePagination(setSize, isValidating, referralsList !== undefined && !referralsList[referralsList.length - 1].next)
 
     return (
