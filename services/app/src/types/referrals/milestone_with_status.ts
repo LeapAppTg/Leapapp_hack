3bfa48082642466094da84cff0adf223462ab051
fedorerd <fedor.timohov@gmail.com>
2024-09-10 04:04:18 +0300

    upd type - rm claimedAt

--- services/app/src/types/referrals/milestone_with_status.ts
+++ services/app/src/types/referrals/milestone_with_status.ts
@@ -3,20 +3,17 @@ import { Milestone, MilestoneApi, MilestoneApiTypeBuilder } from "."
 
 export type MilestoneWithStatusApi = {
     is_claimed: boolean,
-    claimed_at: string,
     milestone: MilestoneApi
 }
 
 export type MilestoneWithStatus = {
     isClaimed: boolean,
-    claimedAt: Date,
     milestone: Milestone
 }
 
 export const MilestoneWithStatusApiTypeBuilder = new ApiTypeBuilder<MilestoneWithStatusApi, MilestoneWithStatus>(
     (i) => ({
         isClaimed: i.is_claimed,
-        claimedAt: new Date(i.claimed_at),
         milestone: MilestoneApiTypeBuilder.convert(i.milestone)
     })
 )
