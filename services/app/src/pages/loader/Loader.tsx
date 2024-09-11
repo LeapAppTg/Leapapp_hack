784bfe52f308707f215ae5614753a52a9f90423a
fedorerd <fedor.timohov@gmail.com>
2024-09-11 22:35:24 +0300

    upd loader (fix unexisting src)

--- services/app/src/pages/loader/Loader.tsx
+++ services/app/src/pages/loader/Loader.tsx
@@ -8,7 +8,7 @@ import styles from "./styles.module.css";
 export const LoaderPage: FC = () => {
 
     const [progress, setProgress] = useState(0)
-    const skins = ["hero_happy", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+    const skins = ["hero_happy", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     const [currentSrc, setSrc] = useState(0)
 
     useEffect(() => {
